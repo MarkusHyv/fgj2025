342f87076f910c53feed76403a758158f6bba316
Cudl <antto.vaatainen@gmail.com>
2025-01-25 20:20:28 +0200

    Ducks

--- Sodascream/Assets/Scripts/DuckRandomizer.cs
+++ Sodascream/Assets/Scripts/DuckRandomizer.cs
@@ -1,27 +1,23 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class DuckRandomizer : MonoBehaviour
 {
     [SerializeField]
-    private List<GameObject> _ducks;
+    private List<RuntimeAnimatorController> _ducks;
     [SerializeField]
-    private Transform _root;
+    private Animator _animator;
+
+    public void OnValidate()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     public void OnEnable()
     {
-        var childCount = _root.childCount;
-        for (var i = 0; i < childCount - 1; i++)
-        {
-            Destroy(_root.GetChild(i).gameObject);
-        }
-
-        var index = Random.Range(0, _ducks.Count - 1);
-        var duck = Instantiate(_ducks[index], _root);
-        duck.transform.localPosition = Vector3.zero;
-        duck.transform.localRotation = Quaternion.identity;
-        duck.transform.localScale = Vector3.one;
-        SetLayerRecursively(duck.transform, LayerMask.NameToLayer("Twerk"));
+        _animator.runtimeAnimatorController = _ducks[Random.Range(0, _ducks.Count - 1)];
     }
 
     public void SetLayerRecursively(Transform t, int layer)
