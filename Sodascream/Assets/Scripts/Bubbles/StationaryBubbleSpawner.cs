0ba63ffdc6b9a176acade1fee91af24ba531099a
Antti L. <a.lipsanen@gmail.com>
2025-01-25 22:16:05 +0200

    Add stationary bubble spawners

---
+++ Sodascream/Assets/Scripts/Bubbles/StationaryBubbleSpawner.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationaryBubbleSpawner : MonoBehaviour
+{
+
+    [SerializeField] private BubbleConfigScriptableObject _bubbleConfigProvider;
+    [SerializeField] private float _spawnIntervalAtStartInSeconds;
+    [SerializeField] private float _spawnIntervalMinimumInSeconds;
+    [SerializeField] private float _timeToReachMinimumSpawnIntervalInSeconds;
+    [SerializeField] private List<BubbleType> _spawnsBubbleTypes;
+    [SerializeField] private float _maximumSpawnDistanceFromOrigin;
+
+    private float _currentSpawnInterval;
+
+    private void Start()
+    {
+        _currentSpawnInterval = _spawnIntervalAtStartInSeconds;
+        StartCoroutine(SpawnBubbleCoroutine());
+    }
+
+    private IEnumerator SpawnBubbleCoroutine()
+    {
+        while (true)
+        {
+            UnityEngine.Debug.Log("Spawning bubble, spawn interval is now: " + _currentSpawnInterval);
+            SpawnRandomBubble();
+            yield return new WaitForSeconds(_currentSpawnInterval);
+        }
+    }
+
+    private void SpawnRandomBubble()
+    {
+        int spawnBubbleTypeIndex = UnityEngine.Random.Range(0, _spawnsBubbleTypes.Count);
+        BubbleType spawnBubbleType = _spawnsBubbleTypes[spawnBubbleTypeIndex];
+        var bubbleConfig = _bubbleConfigProvider.GetBubbleConfig(spawnBubbleType);
+
+        var randomPosition = new Vector3(
+            UnityEngine.Random.Range(-_maximumSpawnDistanceFromOrigin, _maximumSpawnDistanceFromOrigin),
+            0f,
+            UnityEngine.Random.Range(-_maximumSpawnDistanceFromOrigin, _maximumSpawnDistanceFromOrigin)
+        );
+
+        Bubble bubble = Instantiate<Bubble>(_bubbleConfigProvider.BaseBubblePrefab, randomPosition, Quaternion.identity);
+        var randomSizeMultiplier = UnityEngine.Random.Range(bubbleConfig.SizeMultiplier - bubbleConfig.SizeRandomVariance, bubbleConfig.SizeMultiplier);
+        Vector3 randomDirection = new Vector3(UnityEngine.Random.Range(-1f, 1f), 0f, UnityEngine.Random.Range(-1f, 1f)).normalized;
+
+        bubble.Initialize(
+            spawnBubbleType: spawnBubbleType,
+            speed: bubbleConfig.SpeedMultiplier * _bubbleConfigProvider.BaseBubbleSpeed,
+            sizeMultiplier: randomSizeMultiplier,
+            randomDirection: randomDirection,
+            scoreIncrease: bubbleConfig.ScoreValue,
+            bubbleLifeTimeInSeconds: _bubbleConfigProvider.BubbleLifeTimeInSeconds,
+            lifeIncreaseOnBurst: bubbleConfig.BubbleLifeIncrease,
+            lifeDecreaseOnHit: bubbleConfig.BubbleLifeDecrease
+            );
+    }
+
+    private void Update()
+    {
+        _currentSpawnInterval = Mathf.Lerp(_spawnIntervalAtStartInSeconds, _spawnIntervalMinimumInSeconds, Time.time / _timeToReachMinimumSpawnIntervalInSeconds);
+    }
+}
